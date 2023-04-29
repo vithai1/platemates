e787d2ad86c46c945c4eb007d420a1893010c49f
Vivi <vithai1@berkeley.edu>
2023-04-29 01:52:50 -0700

    basic main w/o img

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='head'>
+        <header>
+          <h1>PlateMates</h1>
+        </header>
+      </div>
+      <body>
+        <div className='rec-wrap'>
+          <div className='rec-item'>
+          <h2>Recipe Recommendations</h2>
+          </div>
+        </div>
+        <div className ="cookbook-wrap">
+          <div className='cookbook-item'>
+            <image src= "./Cookbook.png" alt= "cookbook"/>
+            <h2>Cookbook</h2>
+          </div>
+        </div>
+        <div className='calendar-wrap'>
+          <div className='calendar-item'>
+            <image src= "./Calendar.png" alt= "calendar"/>
+            <h2>Calendar</h2>
+          </div>
+        </div>
+      </body>
     </div>
   );
 }
